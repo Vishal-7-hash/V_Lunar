0322baaef3eb7589dd8c0d976690f152f7e2910e
Patel Jahnvi <pateljahanvim20@gmail.com>
2026-08-29 17:56:58 +0530

    add intensity normalization function and integrate into main workflow

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from loader import load_image, normalize
 from Preprocessing.Georeferencing import georeferencing, visualize_georeferencing, visualize_comparison
-
+import cv2
+import matplotlib.pyplot as plt
+from Preprocessing.intensity_normalisation import intensity_normalization
 
 # Load images
-source = load_image("data/source.png")
-reference = load_image("data/reference.png")
+source = load_image(r"C:\Users\JAHANVI\lunar-image-comparison\Preprocessing\SourceImage.png")
+reference = load_image(r"C:\Users\JAHANVI\lunar-image-comparison\Preprocessing\ReferenceImage.jpeg")
 
 print(f"✓ Source image loaded: {source.shape}")
 print(f"✓ Reference image loaded: {reference.shape}")
@@ -30,3 +32,53 @@ visualize_georeferencing(source, reference, source_georeferenced)
 print("Showing overlay comparison...")
 visualize_comparison(reference, source_georeferenced, alpha=0.5)
 
+source_normalized = intensity_normalization(
+    source,
+    low_percentile=2,
+    high_percentile=98
+)
+
+reference_normalized = intensity_normalization(
+    reference,
+    low_percentile=2,
+    high_percentile=98
+)
+
+cv2.imwrite(
+    "source_normalized.png",
+    source_normalized
+)
+
+cv2.imwrite(
+    "reference_normalized.png",
+    reference_normalized
+)
+
+plt.figure(figsize=(12, 6))
+
+
+
+
+# Normalized Source
+plt.subplot(2, 2, 2)
+plt.imshow(source_normalized, cmap="gray")
+plt.title("Normalized Source Image")
+plt.axis("off")
+
+
+# Original Reference
+plt.subplot(2, 2, 3)
+plt.imshow(reference, cmap="gray")
+plt.title("Original Reference Image")
+plt.axis("off")
+
+
+# Normalized Reference
+plt.subplot(2, 2, 4)
+plt.imshow(reference_normalized, cmap="gray")
+plt.title("Normalized Reference Image")
+plt.axis("off")
+
+
+plt.tight_layout()
+plt.show()
